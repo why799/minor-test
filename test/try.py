cb0567af3ee14a6206588353815b783b9c910b08
why799 <853673924@qq.com>
2020-03-12 00:45:17 +0800

    test

--- test/try.py
+++ test/try.py
@@ -4,18 +4,21 @@ def callnum(digits):
     output = [key for key in callnumbers[digits[0]]]
     for index in range(1, len(digits)):
         number = digits[index]
-        if number in [0, 1]:
-            break
         options = []
         for key in callnumbers[number]:
             options += [option + key for option in output]
         output = options
         output.sort()
-    print(output)
+    for i in output:
+        print(i,end=' ')
 
 
 if __name__ == '__main__':
     a = input('Input:\n')
-    a = a.split(',')
+    b = ''
+    for i in range(0, len(a)):
+        if i != 0 and i != len(a)-1 :
+            b = b + a[i]
+    b = b.split(',')
     print('Output:')
-    callnum(a)
+    callnum(b)
--- test/try2.py
+++ test/try2.py
@@ -1,24 +1,17 @@
-def convert_to_mandarin(us_num):
-    trans1 = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight",
-              9: "nine"}
-    trans2 = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen",
-              17: "seventeen", 18: "eighteen", 19: "nineteen", 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty",
-              6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"}
-    if us_num >= 0 and us_num < 10:
-        return trans1[us_num]
-    elif us_num > 9 and us_num < 20:
-        return trans2[us_num]
-    elif us_num > 19 and us_num < 100:
-        if (us_num % 10 == 0):
-            return trans2[us_num // 10]
-        else:
-            tens = trans2[us_num // 10] + ' ' + trans1[us_num % 10]
-            return tens
-    else:
-        print('只支持0-99的数字！')
-        exit()
+trans1={0: "zero",1: "one",2: "two",3: "three", 4: "four",5: "five",6: "six",7: "seven",8: "eight",9: "nine"}
+trans2={10: "ten",11: "eleven",12: "twelve",13: "thirteen",14: "fourteen",15: "fifteen", 16: "sixteen",17: "seventeen",18: "eighteen",19: "nineteen",2: "twenty",3: "thirty",4: "forty",5: "fifty",6: "sixty", 7: "seventy",8: "eighty",9: "ninety"}
+for us_num in range(0,100):
+        if us_num >= 0 and us_num < 10:
+          print(trans1[us_num])
+        elif us_num > 9 and us_num < 20:
+           print(trans2[us_num])
+        elif us_num > 19 and us_num < 100:
+           if(us_num%10==0):
+             print(trans2[us_num//10])
+           else:
+            tens = trans2[us_num//10]+' '+trans1[us_num%10]
+            print(tens)
 
 
-if __name__ == '__main__':
-    n = int(input("输入0-99的数字:"))
-    print(convert_to_mandarin(n))
+
+
